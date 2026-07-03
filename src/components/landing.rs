5bca0ed6d780813da280e4a64a76e07a7b045894
Marcel Scherzinger <>
2026-07-03 16:34:07 +0200

    main: add landing page

--- src/components/landing.rs
+++ src/components/landing.rs
@@ -1,11 +1,8 @@
-use sreport::{
-    messages::MessageAdder,
-    prelude::{Message, TestCase},
-};
+use itertools::Either;
+// use sreport::prelude::{Message, TestCase};
 use yew::prelude::*;
 
-use crate::testcase::TestCaseComponent;
-
+/*
 const MSG1: Message<TestCase> = Message::cinfo(
     "Als ich Ihr Programm aufgerufen habe, hat Ihre Ausgabe einen der anderen abgeprüften Namen enthalten. Deshalb nehme ich an, dass Sie die Aufgabe nicht richtig lösen, sondern die Ergebnisse fest in Ihr Programm kodieren.",
 );
@@ -17,13 +14,63 @@ const MSG2: Message<TestCase> = Message::cwarning(
 const MSG3: Message<TestCase> = Message::cerror(
     "Als ich Ihr Programm aufgerufen habe, hat Ihre Ausgabe einen der anderen abgeprüften Namen enthalten. Deshalb nehme ich an, dass Sie die Aufgabe nicht richtig lösen, sondern die Ergebnisse fest in Ihr Programm kodieren.",
 );
+*/
 
-#[cfg(not(debug_assertions))]
+// #[cfg(not(debug_assertions))]
 #[function_component(LandingPage)]
 pub fn landing_page() -> Html {
-    html!()
+    use Either::Left as L;
+    use Either::Right as R;
+    let name_expl = [
+        (
+            vec![R("Baste"), L("S"), R("t")],
+            "Scratch ist mein Gebiet und das beginnt mit S.",
+        ),
+        (
+            vec![L("B"), R("ast"), L("3S"), R("t")],
+            "Die Dateiendung von Scratch ist sb3.",
+        ),
+        (
+            vec![L("Baste"), R("S"), L("t")],
+            "Bastet ist die ägyptische Katzengöttin und Scratch ist für seine Katze bekannt.",
+        ),
+        (
+            vec![R("Bas"), L("teSt")],
+            "Ich bin dazu da, um Ihre Abgaben zu testen.",
+        ),
+    ];
+
+    let name_comp = name_expl.iter().map(|(v, desc)| {
+        let h = v.iter().map(|n| match n {
+            L(highlight) => html!(<span class="name-highlight">{highlight}</span>),
+            R(unimport) => html!(<span class="name-unimportant">{unimport}</span>),
+        });
+        html!(<tr><td>{for h}{": "}</td><td>{desc}</td></tr>)
+    });
+
+    html!(
+        <main>
+            <div>
+                <h1 style={"margin: auto; text-align: center;"}>
+                    {"Willkommen zu Bast3St!"}
+                </h1>
+                <p class="welcome-info">{"Ich bin dazu da, um Ihnen bei Ihren Scratch-Abgaben zu helfen. Sie können mir Ihre Dateien geben und ich teste Sie für die jewiligen Aufgaben mit verschiedenen Eingaben. Wenn alles passt wissen, Sie auf diese Weise sofort, dass Ihre Lösung ausreichend ist. Ab und zu kann ich Ihnen auch Tipps bei Fehlern geben, sofern ich erkenne, worin das Problem besteht."}</p>
+                <p class="welcome-info">{"Noch befinde ich mich in der Versuchsphase. Wenn Ihnen also etwas auffällt, das ich besser machen sollte, oder wenn Sie mit einem Hinweis nicht zurechtkommen, zögern Sie nicht, über das Forum nachzufragen."}</p>
+
+                <section class="welcome-info">
+                <p>{"Mein Name ist sehr vielschichtig und funktioniert auf mehreren Ebenen:"}</p>
+                <table class="name-info">
+                    {for name_comp}
+                </table>
+                </section>
+
+
+            </div>
+        </main>
+    )
 }
 
+/*
 #[cfg(debug_assertions)]
 #[function_component(LandingPage)]
 pub fn landing_page() -> Html {
@@ -73,3 +120,4 @@ pub fn landing_page() -> Html {
         </main>
     )
 }
+*/
